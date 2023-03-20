16a20bf0867fa392d496026b2df8622ed689d27b
X-I-A <develop@x-i-a.com>
2023-03-20 23:39:16 +0100

    feat: adding firestore

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_project_service" "firestore" {
 }
 
 resource "google_firestore_database" "database" {
-  project     = var.project_id
+  project     = data.google_client_config.current.project
   name        = "(default)"
   location_id = var.firestore_location
   type        = "FIRESTORE_NATIVE"
@@ -28,34 +28,34 @@ resource "google_firestore_database" "database" {
 
 resource "google_service_account" "xia_terraform" {
   account_id = var.terraform_user
-  project = var.project_id
+  project = data.google_client_config.current.project
 
   depends_on = [google_project_service.iam_api]
 }
 
 resource "google_project_iam_binding" "security_admin_binding" {
-  project = var.project_id
+  project = data.google_client_config.current.project
   role    = "roles/securitycenter.admin"
   members = ["serviceAccount:${google_service_account.xia_terraform.email}"]
   depends_on = [google_service_account.xia_terraform]
 }
 
 resource "google_project_iam_binding" "service_usage_admin_binding" {
-  project = var.project_id
+  project = data.google_client_config.current.project
   role    = "roles/serviceusage.serviceUsageAdmin"
   members = ["serviceAccount:${google_service_account.xia_terraform.email}"]
   depends_on = [google_service_account.xia_terraform]
 }
 
 resource "google_project_iam_binding" "service_account_admin_binding" {
-  project = var.project_id
+  project = data.google_client_config.current.project
   role    = "roles/iam.serviceAccountAdmin"
   members = ["serviceAccount:${google_service_account.xia_terraform.email}"]
   depends_on = [google_service_account.xia_terraform]
 }
 
 resource "google_project_iam_binding" "service_account_user_binding" {
-  project = var.project_id
+  project = data.google_client_config.current.project
   role    = "roles/iam.serviceAccountUser"
   members = ["serviceAccount:${google_service_account.xia_terraform.email}"]
   depends_on = [google_service_account.xia_terraform]
