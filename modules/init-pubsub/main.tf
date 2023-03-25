91096158e128885f7bee46ddacc912e33d588300
X-I-A <develop@x-i-a.com>
2023-03-25 21:26:31 +0100

    feat: pubsub agent user should be able to push / pull data to pubsub topics

--- modules/init-pubsub/main.tf
+++ modules/init-pubsub/main.tf
@@ -15,3 +15,9 @@ resource "google_project_iam_member" "project_token_creator" {
   role    = "roles/iam.serviceAccountTokenCreator"
   member  = "serviceAccount:${google_project_service_identity.pubsub_agent.email}"
 }
+
+resource "google_project_iam_member" "project_pubsub_admin" {
+  project = var.project_id
+  role    = "roles/pubsub.admin"
+  member  = "serviceAccount:${google_project_service_identity.pubsub_agent.email}"
+}
